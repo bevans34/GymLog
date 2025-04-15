b6c9519dfd162c7f4bd037f001737056dc25e7de
Brandon Evans <0595069@my.msjc.edu>
2025-04-15 17:28:01 -0600

    Added rest of Video08 updates

--- app/src/main/java/com/example/gymlog/MainActivity.java
+++ app/src/main/java/com/example/gymlog/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.gymlog;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
@@ -16,6 +18,8 @@ import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = "GymLog";
+    private static final String MAIN_ACTIVITY_USER_ID = "com.example.gymlog.MAIN_ACTIVITY_USER_ID";
+
     ActivityMainBinding binding;
     private GymLogRepository repository;
 
@@ -29,6 +33,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        loginUser();
+
+        if (loggedInUserId == -1) {
+            Intent intent = LoginActivity.loginIntentFactory(getApplicationContext());
+            startActivity(intent);
+        }
 
         repository = GymLogRepository.getRepository(getApplication());
 
@@ -46,6 +57,17 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    private void loginUser() {
+        // TODO: Implement loginUser method
+        loggedInUserId = getIntent().getIntExtra(MAIN_ACTIVITY_USER_ID, -1);
+    }
+
+    static Intent mainActivityIntentFactory(Context context, int userId) {
+        Intent intent = new Intent(context, MainActivity.class);
+        intent.putExtra(MAIN_ACTIVITY_USER_ID, userId);
+        return intent;
+    }
+    
     private void getInformationFromDisplay() {
         this.mExercise = binding.exerciseInputEditText.getText().toString();
 
