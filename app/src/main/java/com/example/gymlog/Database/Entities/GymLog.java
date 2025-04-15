65d833e161454623031738f74f2cd26a2c9e8815
Brandon Evans <0595069@my.msjc.edu>
2025-04-15 14:11:43 -0600

    Implemented GymLog.java

---
+++ app/src/main/java/com/example/gymlog/Database/Entities/GymLog.java
@@ -0,0 +1,85 @@
+/*
+ * Author: Brandon Evans
+ * File: GymLog.java
+ * Date: 4/15/2025
+ * Description: This class acts as an instance of a GymLog database.
+ */
+
+package com.example.gymlog.Database.Entities;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Entity(tableName = "gymLog")
+public class GymLog {
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    private String exercise;
+    private double weight;
+    private int reps;
+    private LocalDate date;
+
+    public GymLog(String exercise, double weight, int reps) {
+        this.exercise = exercise;
+        this.weight = weight;
+        this.reps = reps;
+        this.date = LocalDate.now();
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public int getReps() {
+        return reps;
+    }
+
+    public void setReps(int reps) {
+        this.reps = reps;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public String getExercise() {
+        return exercise;
+    }
+
+    public void setExercise(String exercise) {
+        this.exercise = exercise;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GymLog gymLog = (GymLog) o;
+        return id == gymLog.id && Double.compare(weight, gymLog.weight) == 0 && reps == gymLog.reps && Objects.equals(exercise, gymLog.exercise) && Objects.equals(date, gymLog.date);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, exercise, weight, reps, date);
+    }
+}
