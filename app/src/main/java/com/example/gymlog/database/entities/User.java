3b61e956c33b504eb8a885e8f9f1e60678969b7b
Brandon Evans <0595069@my.msjc.edu>
2025-04-15 18:24:20 -0600

    Still somehow messed it up. Should be good now

--- app/src/main/java/com/example/gymlog/database/entities/User.java
+++ app/src/main/java/com/example/gymlog/database/entities/User.java
@@ -22,7 +22,7 @@ public class User {
     private String password;
     private boolean isAdmin;
 
-    public User(String username, String passwors) {
+    public User(String username, String password) {
         this.username = username;
         this.password = password;
         this.isAdmin = false;
